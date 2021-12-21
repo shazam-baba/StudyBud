5522d5eba581f6f9ca923d6068f937038d2fde57
Siddharh Sharma <62809024+shazam-baba@users.noreply.github.com>
2021-12-21 06:06:49 -0800

    chat-app

--- base/models.py
+++ base/models.py
@@ -6,7 +6,6 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
-
     avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
